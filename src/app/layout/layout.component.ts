65a637a53520be1cb1aba262de7c8a784c325d80
Dhiraj <dhiraj@bytemark.co>
2022-08-04 14:00:18 +0545

    layout made

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,7 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core';
+import { interval, Observable, Observer } from 'rxjs';
 import { getDocs } from 'firebase/firestore';
 import { AppService } from '../app.service';
 import { CURRENCY_LIST } from '../currency';
+
+const PROGRESS_COUNT = 25;
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -23,11 +26,27 @@ export class LayoutComponent implements OnInit {
   currentIndex = 0;
   page = 0;
   currentArray: any = [];
+  progressValue = 25;
+  minutes = 0;
+  seconds = 0;
+
+  taskGroup = 4;
+  taskIndex = 1;
+
+  formError = {
+    amount: false,
+    currency: false,
+    month: false,
+    day: false,
+    year: false,
+  };
+  time: Observable<string> | null = null;
 
   constructor(private renderer: Renderer2, private appService: AppService) {}
 
   ngOnInit(): void {
     this.getItems();
+    this.showTimer();
   }
 
   async getItems() {
@@ -81,18 +100,52 @@ export class LayoutComponent implements OnInit {
     }
   }
 
+  showTimer() {
+    this.time = new Observable<string>((observer: Observer<string>) => {
+      var fiveMinutes = 60 * 5;
+      let timer = fiveMinutes,
+        minutes,
+        seconds;
+      setInterval(function () {
+        minutes = parseInt((timer / 60).toString(), 10);
+        seconds = parseInt((timer % 60).toString(), 10);
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        if (--timer < 0) {
+          timer = fiveMinutes;
+        }
+        return observer.next(`${minutes.toString()}:${seconds.toString()}`);
+      }, 1000);
+    });
+  }
+
+  nextTask() {
+    this.taskIndex++;
+    this.currentObject = this.billsArrayList[this.currentIndex++];
+    this.progressValue = PROGRESS_COUNT * this.taskIndex;
+    this.currentObject = this.billsArrayList[++this.currentIndex];
+  }
+
   validateData() {
+    if (this.taskIndex === this.taskGroup) {
+      this.taskIndex = 1;
+      this.progressValue = 25;
+      this.showTimer();
+    }
     if (this.currentObject.type === 'amount') {
+      this.formError.amount = this.amount === '' ? true : false;
+      this.formError.currency = this.currency === '' ? true : false;
       if (
         this.currentObject.amount === this.amount &&
         this.currentObject.currency === this.currency
       ) {
-        this.currentObject = this.billsArrayList[this.currentIndex++];
+        this.nextTask();
       }
     } else if (this.currentObject.type === 'date') {
       const date = this.month + '/' + this.date + '/' + this.year;
       if (this.currentObject.date === date) {
-        this.currentObject = this.billsArrayList[this.currentIndex++];
+        this.nextTask();
       }
     }
   }
